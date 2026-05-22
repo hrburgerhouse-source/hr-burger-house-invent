13f7f0cdca1c83cedaa096d0ec5fc8759ebe62c3
hrburgerhouse <hrburgerhouse@gmail.com>
2026-05-22 14:30:35 -0600

    agregar credenciales Firebase

--- js/config.js
+++ js/config.js
@@ -6,12 +6,12 @@
 //   3. Copia las credenciales aquí:
 // ================================================================
 const firebaseConfig = {
-  apiKey:            "PEGA_TU_API_KEY",
-  authDomain:        "PEGA_TU_AUTH_DOMAIN",
-  projectId:         "PEGA_TU_PROJECT_ID",
-  storageBucket:     "PEGA_TU_STORAGE_BUCKET",
-  messagingSenderId: "PEGA_TU_MESSAGING_SENDER_ID",
-  appId:             "PEGA_TU_APP_ID"
+  apiKey:            "AIzaSyDXLhnKJ6AKPBw0z6cEEOHbvoJYbwBGP6g",
+  authDomain:        "hr-burger-house-invent.firebaseapp.com",
+  projectId:         "hr-burger-house-invent",
+  storageBucket:     "hr-burger-house-invent.firebasestorage.app",
+  messagingSenderId: "571449659906",
+  appId:             "1:571449659906:web:88eeb364f01ee0b7cfcd1b"
 };
 
 // ================================================================
